d29488c6d876e37da7f0e68ad913ff68a6075d5b
Elena Volkova <lenochka.volkova@gmail.com>
2025-04-28 13:21:47 +0400

    last_paid_click

--- last_paid_click.sql
+++ last_paid_click.sql
@@ -1,52 +1,47 @@
-with paid_sessions as (
+/*
+amount — от большего к меньшему, null записи идут последними
+visit_date — от ранних к поздним
+utm_source, utm_medium, utm_campaign — в алфавитном порядкеl */
+with paid_clicks as (
 	select 
-		sessions.visitor_id,
-		sessions.source as utm_source,
-		sessions.medium as utm_medium,
-		sessions.campaign as utm_campaign,
-		sessions.visit_date
-	from sessions
-	left join leads on sessions.visitor_id = leads.visitor_id
-	where sessions.medium != 'organic' and sessions.visit_date <= leads.created_at
-),
-last_paid_sessions as (
-	select 
-		distinct visitor_id,
-		last_value(utm_source) over (
+		s.visitor_id,
+		s.visit_date,
+		s.source as utm_source,
+		s.medium as utm_medium,
+		s.campaign as utm_campaign,
+		l.lead_id,
+		l.created_at,
+		l.amount,
+		l.closing_reason,
+		l.status_id
+	from sessions s
+	join leads l on s.visitor_id = l.visitor_id
+	where s.medium in ('cpc', 'cpm', 'cpa', 'youtube', 'cpp', 'tg', 'social')
+		and s.visit_date <= l.created_at
+)
+select 
+	distinct visitor_id,
+	last_value(visit_date) over (
 			partition by visitor_id
 			order by visit_date
-			) as utm_source,
-		last_value(utm_medium) over (
+			) as visit_date,
+	last_value(utm_source) over (
+			partition by visitor_id
+			order by visit_date
+			) as utm_source,	
+	last_value(utm_medium) over (
 			partition by visitor_id
 			order by visit_date
 			) as utm_medium,
-		last_value(utm_campaign) over (
+	last_value(utm_campaign) over (
 			partition by visitor_id
 			order by visit_date
 			) as utm_campaign,
-		last_value(visit_date) over (
-			partition by visitor_id
-			order by visit_date
-			) as visit_date
-	from paid_sessions
-)
-select 
-	distinct sessions.visitor_id,
-	last_paid_sessions.visit_date,
-	last_paid_sessions.utm_source,
-	last_paid_sessions.utm_medium,
-	last_paid_sessions.utm_campaign,
-	leads.lead_id,
-	leads.created_at,
-	leads.amount,
-	leads.closing_reason,
-	leads.status_id
-from sessions
-left join leads on sessions.visitor_id = leads.visitor_id
-join last_paid_sessions
-	on sessions.visitor_id = last_paid_sessions.visitor_id
-order by leads.amount desc nulls last,
-	last_paid_sessions.visit_date,
-	last_paid_sessions.utm_source, 
-	last_paid_sessions.utm_medium,
-	last_paid_sessions.utm_campaign;
+	lead_id,
+	created_at,
+	amount,
+	closing_reason,
+	status_id
+from paid_clicks
+order by amount desc nulls last, visit_date, utm_source, utm_medium, utm_campaign;
+
