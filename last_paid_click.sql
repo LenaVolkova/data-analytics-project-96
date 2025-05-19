f83fea2b09e97dc22b535fdba7bd3575a87ccbdd
Elena Volkova <lenochka.volkova@gmail.com>
2025-05-19 18:32:00 +0400

    fixes

--- last_paid_click.sql
+++ last_paid_click.sql
@@ -1,5 +1,5 @@
 with paid_clicks as (
-    select
+    select 
         s.visitor_id,
         s.visit_date,
         s.source as utm_source,
@@ -9,37 +9,32 @@ with paid_clicks as (
         l.created_at,
         l.amount,
         l.closing_reason,
-        l.status_id
+        l.status_id, 
+        row_number()
+            over (partition by s.visitor_id
+            order by visit_date desc) as rn        
     from sessions as s
     left join
         leads as l
         on s.visitor_id = l.visitor_id and s.visit_date <= l.created_at
     where s.medium in ('cpc', 'cpm', 'cpa', 'youtube', 'cpp', 'tg', 'social')
-),
-
-last_date as (
-    select
-        visitor_id,
-        max(visit_date) as last_visit_date
-    from paid_clicks
-    group by visitor_id
 )
-
-select
-    last_date.visitor_id,
-    last_date.last_visit_date as visit_date,
-    paid_clicks.utm_source,
-    paid_clicks.utm_medium,
-    paid_clicks.utm_campaign,
-    paid_clicks.lead_id,
-    paid_clicks.created_at,
-    paid_clicks.amount,
-    paid_clicks.closing_reason,
-    paid_clicks.status_id
+select 
+    visitor_id,
+    visit_date,
+    utm_source,
+    utm_medium,
+    utm_campaign,
+    lead_id,
+    created_at,
+    amount,
+    closing_reason,
+    status_id
 from paid_clicks
-inner join last_date on paid_clicks.visitor_id = last_date.visitor_id
-where paid_clicks.visit_date = last_date.last_visit_date
+where rn = 1
 order by
-    paid_clicks.amount desc nulls last,
-    visit_date asc, paid_clicks.utm_source asc,
-    paid_clicks.utm_medium asc, paid_clicks.utm_campaign asc;
+    amount desc nulls last,
+    visit_date asc,
+    utm_source asc,
+    utm_medium asc,
+    utm_campaign asc;
